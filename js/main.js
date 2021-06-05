08b3481280088a51e14b017a5db1fa8ad2cbd18d
Zoy <zjgolub@gmail.com>
2021-06-05 05:44:37 +0300

    Исправила код по вычислению случайного числа и длинны строки

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-const CHARACTERS = 140;
-let randomInteger;
-let n = 1;
+const MAX_SYMBOL = 140;
+const line = 'aaaaa';
 
-const randomSelection = function(min, max) {
-  if (max <= min) {
-    max++;
+const getRandomNumber = function(min, max) {
+  if (max > min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    return false ;
   }
-
-  return randomInteger(min, max);
 };
 
-randomSelection(3, 7);
+getRandomNumber(0, 43);
 
-const maxLenght = function(lineNumber, lineLenght) {
-  return n <= lineNumber && lineLenght <= CHARACTERS ? n++ : 'error';
+// console.log(getRandomNumber(-43, 12));
+
+const maxLenght = function(_str, maxStrLenght) {
+  if (maxStrLenght <= MAX_SYMBOL) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-maxLenght(3, 57);
+maxLenght(line, MAX_SYMBOL);
+
+// console.log(maxLenght(line, MAX_SYMBOL));
