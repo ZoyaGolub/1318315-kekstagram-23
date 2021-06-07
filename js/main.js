8822554b7995f153fe41b3cab9e96b38fcbd601f
Zoy <zjgolub@gmail.com>
2021-06-07 17:33:46 +0300

    Исправила код на тернальный оператор по вычислению случайного числа и длинны строки

--- js/main.js
+++ js/main.js
@@ -2,25 +2,28 @@ const MAX_SYMBOL = 140;
 const line = 'aaaaa';
 
 const getRandomNumber = function(min, max) {
-  if (max > min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {
-    return false ;
-  }
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  const resultNumber = max > min ? randomNumber : Number(false);
+  //console.log(result);
+  return resultNumber;
+  // if (max > min) {
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // } else {
+  //   return false;
+  // }
 };
 
-getRandomNumber(0, 43);
-
-// console.log(getRandomNumber(-43, 12));
+getRandomNumber(100, 43);
 
 const maxLenght = function(_str, maxStrLenght) {
-  if (maxStrLenght <= MAX_SYMBOL) {
-    return true;
-  } else {
-    return false;
-  }
+  const resultMaxLenght = maxStrLenght <= MAX_SYMBOL ? 'ok' : 'bad';
+  // console.log(resultMaxLenght);
+  return resultMaxLenght;
+  // if (maxStrLenght <= MAX_SYMBOL) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 };
 
 maxLenght(line, MAX_SYMBOL);
-
-// console.log(maxLenght(line, MAX_SYMBOL));
