3758fe5c1861491ece320c5e961fef89d187bb34
Zoy <zjgolub@gmail.com>
2021-06-07 18:08:04 +0300

    Тестирование кода

--- js/main.js
+++ js/main.js
@@ -3,27 +3,17 @@ const line = 'aaaaa';
 
 const getRandomNumber = function(min, max) {
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  const resultNumber = max > min ? randomNumber : Number(false);
-  //console.log(result);
-  return resultNumber;
-  // if (max > min) {
-  //   return Math.floor(Math.random() * (max - min + 1)) + min;
-  // } else {
-  //   return false;
-  // }
+  const resultRandomNumber = max > min ? randomNumber : Number(false);
+  // console.log(resultRandomNumber);
+  return resultRandomNumber;
 };
 
-getRandomNumber(100, 43);
+getRandomNumber(0, 25);
 
-const maxLenght = function(_str, maxStrLenght) {
-  const resultMaxLenght = maxStrLenght <= MAX_SYMBOL ? 'ok' : 'bad';
+const maxLenght = function(str, maxStrLenght) {
+  const resultMaxLenght = str.lenght <= MAX_SYMBOL;
   // console.log(resultMaxLenght);
   return resultMaxLenght;
-  // if (maxStrLenght <= MAX_SYMBOL) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
 };
 
 maxLenght(line, MAX_SYMBOL);
