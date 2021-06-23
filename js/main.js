165515ea9d6d02abde7c65f0d124c9ebf2264e31
Zoy <zjgolub@gmail.com>
2021-06-23 08:41:48 +0300

    Создание массива описания фотографий

--- js/main.js
+++ js/main.js
@@ -17,3 +17,63 @@ const maxLenght = function(str, maxStrLenght) {
 };
 
 maxLenght(line, MAX_SYMBOL);
+
+const MASSADE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAME = [
+  'Георгий',
+  'Мария',
+  'Виктория',
+  'Леонид',
+  'Анастасия',
+  'Максим',
+  'Александр',
+  'Константин',
+  'Матвей',
+  'Георгий',
+  'Иван',
+  'Роман',
+  'Артём',
+  'Фёдор',
+  'Екатерина',
+  'Ксения',
+  'Алексей',
+  'Полина',
+  'Анна',
+  'Диана',
+  'Варвара',
+  'София',
+  'Кира',
+  'Вера',
+  'Вероника',
+];
+
+const getRandomNumberAvatar = getRandomNumber(1, 6);
+
+const comment = {
+  id: getRandomNumber(1, 500),
+  avatar: 'img/avatar-getRandomNumberAvatar.svg',
+  massage: getRandomNumber(MASSADE),
+  name: getRandomNumber(NAME),
+};
+
+const newComment = comment;
+
+const photo = {
+  id: getRandomNumber(1, 25),
+  url: 'photos/id.jpg',
+  description: '',
+  likes: getRandomNumber(15, 200),
+  comments: Array(comment, newComment),
+};
+
+const newPhoto = photo;
+
+const photos = Array(photo, newPhoto);
